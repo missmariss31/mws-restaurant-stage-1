84b81a7bbf76dc0f12b60f42e97d0be169eec68a
Marissa <marissaschmucker@gmail.com>
2018-10-13 14:43:07 -0500

    add mapbox to service worker

--- sw.js
+++ sw.js
@@ -20,6 +20,26 @@ for (i=1;i<11;i++) {
   cachedFiles.push(`restaurant.html?id=${i}`);
 }
 
+// list of some map coordinates for each page
+var mapCoordinates = [
+  {x: '19300', y: '24639'},
+  {x: '19304', y: '24646'},
+  {x: '19301', y: '24632'},
+  {x: '19300', y: '24638'},
+  {x: '19310', y: '24642'},
+  {x: '19291', y: '24650'},
+  {x: '19301', y: '24635'},
+  {x: '19294', y: '24637'},
+  {x: '19303', y: '24633'},
+  {x: '19306', y: '24633'}
+];
+
+// add mapbox urls to cachedFiles for offline use
+mapCoordinates.map(function(c) {
+  cachedFiles.push(`https://api.tiles.mapbox.com/v4/mapbox.streets/16/${c.x}/${c.y}.jpg70?access_token=pk.eyJ1IjoibXNjaG11Y2tlcjMxIiwiYSI6ImNqbXVyZmQzdTMwMWEza21sc25zMm5lcngifQ.87ivMGNm3z_8SVlpNuF0Zw`);
+});
+
+
 // cache all files
 self.addEventListener('install', function(event) {
   event.waitUntil(
